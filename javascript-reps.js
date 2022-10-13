a370847fdd303813acd3b3506e60013508813cdf
Jerry Martinez <jaysavageg@gmail.com>
2022-10-13 14:21:50 -0400

    Wild Wild Life answered

--- javascript-reps.js
+++ javascript-reps.js
@@ -1,25 +1,44 @@
 // Part II: JavaScript Reps
 
-for (let i = 0; i <= 20; i++) {
-    console.log (i)
-}
+// for (let i = 0; i <= 20; i++) {
+//     console.log (i)
+// }
 
 // For Loop - only EVEN numbers - 0 through 200
 
-for (let i = 0; i <= 200; i+=2) {
-    console.log (i)
-}
+// for (let i = 0; i <= 200; i+=2) {
+//     console.log (i)
+// }
 
 // Fizzbuzz
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log (' Fizzbuzz')
-    } else if (i % 5 === 0) {
-        console.log ('Buzz')
-    } else if (i % 3 === 0) {
-        console.log ('Fizz')
-    }
-}
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log (' Fizzbuzz')
+//     } else if (i % 5 === 0) {
+//         console.log ('Buzz')
+//     } else if (i % 3 === 0) {
+//         console.log ('Fizz')
+//     }
+// }
 
 
+// Wild Wild Life
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = 5001
+// console.log (plantee)
+
+wolfy[3] = 'Gotham City'
+// console.log ('wolfy')
+
+dart.push ('Hawkins')
+// console.log (dart)
+
+wolfy[0] = 'Gameboy'
+// console.log (wolfy)
